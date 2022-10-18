f50f13cd306f321e5158fc048e0f7d872c3ec897
Suzy Liu <43913437+Suzylxx@users.noreply.github.com>
2022-10-17 20:14:46 -0400

    Update 771. Jewels and Stones.java

--- 771. Jewels and Stones.java
+++ 771. Jewels and Stones.java
@@ -1,1 +1,23 @@
-
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        HashMap<Character, Integer> jewelsMap = new HashMap<>();
+        HashMap<Character, Integer> stonesMap = new HashMap<>();
+        int res = 0;
+        
+        for (int i = 0; i < jewels.length(); i++) {
+            jewelsMap.put(jewels.charAt(i), jewelsMap.getOrDefault(jewels.charAt(i), 0) + 1);
+        }
+        
+        for (int i = 0; i < stones.length(); i++) {
+            stonesMap.put(stones.charAt(i), stonesMap.getOrDefault(stones.charAt(i), 0) + 1);
+        }
+        
+        for (Character c : stonesMap.keySet()) {
+            if (jewelsMap.containsKey(c)) {
+                res += stonesMap.get(c);
+            }
+        }
+        
+        return res;
+    }
+}
