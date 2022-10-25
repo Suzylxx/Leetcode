9985058757ee949fbf50c5d38472dee1b260236d
Suzy Liu <43913437+Suzylxx@users.noreply.github.com>
2022-10-25 19:10:23 -0400

    Update 946. Validate Stack Sequences.java

--- 946. Validate Stack Sequences.java
+++ 946. Validate Stack Sequences.java
@@ -1,1 +1,20 @@
+//Brute Force
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int N = pushed.length;
+        Stack<Integer> stack = new Stack();
+
+        int j = 0;
+        for (int x: pushed) {
+            stack.push(x);
+            while (!stack.isEmpty() && j < N && stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+
+        return j == N;
+    }
+}
+
 
