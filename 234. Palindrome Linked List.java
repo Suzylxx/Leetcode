1131e1d3345f1e4c4767082b11d6fe23fe6453f5
Suzy Liu <43913437+Suzylxx@users.noreply.github.com>
2022-11-15 20:20:27 -0500

    Update 234. Palindrome Linked List.java

--- 234. Palindrome Linked List.java
+++ 234. Palindrome Linked List.java
@@ -1,3 +1,4 @@
+//Solution1
 class Solution {
 
     public boolean isPalindrome(ListNode head) {
@@ -42,5 +43,33 @@ class Solution {
             slow = slow.next;
         }
         return slow;
+    }
+}
+
+//Solution2
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> vals = new ArrayList<>();
+
+        // Convert LinkedList into ArrayList.
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            vals.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+
+        // Use two-pointer technique to check for palindrome.
+        int front = 0;
+        int back = vals.size() - 1;
+        while (front < back) {
+            // Note that we must use ! .equals instead of !=
+            // because we are comparing Integer, not int.
+            if (!vals.get(front).equals(vals.get(back))) {
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
     }
 }
