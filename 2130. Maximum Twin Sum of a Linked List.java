b756c3476d68942e4f184b6119d3bbf5a7802ac6
Suzy Liu <43913437+Suzylxx@users.noreply.github.com>
2022-11-15 19:39:43 -0500

    Update 2130. Maximum Twin Sum of a Linked List.java

--- 2130. Maximum Twin Sum of a Linked List.java
+++ 2130. Maximum Twin Sum of a Linked List.java
@@ -1,1 +1,37 @@
-
+import java.util.*;
+import java.lang.*;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        ArrayList<Integer> first = new ArrayList<>();
+        ArrayList<Integer> second  = new ArrayList<>();
+        ListNode firstNode = head;
+        ListNode secondNode = head;
+        while (secondNode != null && secondNode.next != null) {
+            first.add(firstNode.val);
+            firstNode = firstNode.next;
+            secondNode = secondNode.next.next;
+        }
+        secondNode = firstNode;
+        while (secondNode != null) {
+            second.add(secondNode.val);
+            secondNode = secondNode.next;
+        }
+        Collections.reverse(second);
+        int max = 0;
+        for (int i = 0; i < first.size(); i++) {
+            int sum = first.get(i) + second.get(i);
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
