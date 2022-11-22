48b9165038d37cc6fbc1900e283b36bad8aa2aec
Suzy Liu <43913437+Suzylxx@users.noreply.github.com>
2022-11-21 19:53:24 -0500

    Update 1248. Count Number of Nice Subarrays.java

--- 1248. Count Number of Nice Subarrays.java
+++ 1248. Count Number of Nice Subarrays.java
@@ -1,1 +1,29 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        if (k > nums.length) {
+            return 0;
+        }
+        int res = 0;
+        int left = 0;
+        int right = 0;
+        int count = 0;
+        int temp = 0;
 
+        while (right < nums.length && left  < nums.length) {
+            if (nums[right] % 2 == 1) {
+                count += 1;
+                temp = 0;
+            }
+            while (count == k) {
+                temp += 1;
+                if (nums[left] % 2 == 1) {
+                    count -= 1;
+                }
+                left += 1;
+            } 
+            res += temp;
+            right += 1;
+        }
+        return res;
+    }
+}
